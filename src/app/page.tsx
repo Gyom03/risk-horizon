fc40964928e0ad495ec71bc46698186ea6426e2e
Gyom03 <guillaumerichard03@gmail.com>
2024-04-20 20:41:56 +0200

    added images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -81,6 +81,9 @@ export default function Component() {
               </div>
             </div>
           </div>
+          <div className="h-[500px] py-20 w-full">
+            <LayoutGrid cards={cards} />
+          </div>
         </section>
         {/* <section className="w-full py-12 md:py-24 lg:py-32">
           <div className="container grid items-center gap-6 px-4 md:px-6 lg:grid-cols-2 lg:gap-10">
@@ -269,3 +272,24 @@ const SkeletonFour = () => {
     </div>
   )
 }
+
+const cards = [
+  {
+    id: 1,
+    content: <SkeletonTwo />,
+    className: "col-span-1",
+    thumbnail: "/imgs/12.jpg",
+  },
+  {
+    id: 3,
+    content: <SkeletonTwo />,
+    className: "col-span-1",
+    thumbnail: "/imgs/8.jpg",
+  },
+  {
+    id: 2,
+    content: <SkeletonOne />,
+    className: "col-span-1",
+    thumbnail: "/imgs/14.webp",
+  },
+]
