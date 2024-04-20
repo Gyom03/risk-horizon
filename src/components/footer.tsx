255d4b6f77293c5e9cde2f446868eb23f04ba66c
Gyom03 <guillaumerichard03@gmail.com>
2024-04-20 20:41:56 +0200

    added images

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,15 +4,15 @@ import Link from "next/link"
 function Footer() {
   return (
     <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t sm:max-h-6">
-      <p className="text-xs text-gray-500 dark:text-gray-400">© 2024 Acme Inc. All rights reserved.</p>
-      <nav className="sm:ml-auto flex gap-4 sm:gap-6">
+      <p className="text-xs text-gray-500 dark:text-gray-400">© 2024 Risk Horizon.</p>
+      {/* <nav className="sm:ml-auto flex gap-4 sm:gap-6">
         <Link className="text-xs hover:underline underline-offset-4 text-gray-500" href="#">
           Terms of Service
         </Link>
         <Link className="text-xs hover:underline underline-offset-4 text-gray-500" href="#">
           Privacy
         </Link>
-      </nav>
+      </nav> */}
     </footer>
   )
 }
