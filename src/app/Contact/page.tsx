a7634b53111b3c59726499c9f0ce057b86369b79
Rosin_Guillaume <142811942+guillaumerosin@users.noreply.github.com>
2024-04-20 14:16:42 +0200

    GuillaumeFrontEnd1-20.04.24

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -13,20 +13,20 @@ function Page() {
           <div className="container grid items-center justify-center gap-4 px-4 text-center md:px-6">
             <div className="space-y-3 text-gray-50">
               <h2 className="text-3xl font-bold tracking-tighter md:text-4xl/tight">
-                You want to secure your company ?
+              Vous souhaitez une assistance experte pour mettre en place une sécurité optimale pour votre entreprise?
               </h2>
-              <h2 className="text-3xl font-bold tracking-tighter md:text-4xl/tight">Contact Us !</h2>
+              <h2 className="text-3xl font-bold tracking-tighter md:text-4xl/tight"></h2>
               <p className="mx-auto max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
-                Reach out to learn more about our programs and services.
+              Contactez-nous pour en savoir plus sur nos programmes et services.
               </p>
             </div>
             <div className="mx-auto w-full max-w-[400px] space-y-4">
               <form className="grid gap-4">
-                <Input placeholder="Name" type="text" />
+                <Input placeholder="Nom" type="text" />
                 <Input placeholder="Email" type="email" />
-                <Input placeholder="Subject" type="text" />
-                <Textarea placeholder="Your Message" />
-                <Button type="submit">Send</Button>
+                <Input placeholder="Sujet" type="text" />
+                <Textarea placeholder="Votre message" />
+                <Button type="submit">Soumettre</Button>
               </form>
             </div>
           </div>
