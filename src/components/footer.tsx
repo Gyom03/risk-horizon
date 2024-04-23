5b1ee25ba40852686d4ba0d8ca6464564fce4604
Guillaume Richard <81857460+Gyom03@users.noreply.github.com>
2024-04-23 12:38:06 +0200

    added content in contact

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,8 +7,10 @@ import linkedin from "../public/linkedin.svg"
 function Footer() {
   return (
     <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t sm:max-h-6">
-      <p className="text-xs text-gray-500 dark:text-gray-400">© 2024 Risk Horizon.</p>
-      <nav className="sm:ml-auto flex gap-4 sm:gap-6">
+      <p className="text-xs text-gray-500 dark:text-gray-400">© 2024 Risk Horizon ASBL.</p>
+
+      <nav className="sm:ml-auto flex gap-4 sm:gap-6 justify-center items-center">
+        <p className="text-xs text-gray-500 dark:text-gray-400 ml-20">N° d'entreprise : 1008.675.581</p>
         <Link
           className="text-xs hover:underline underline-offset-4 text-gray-500"
           href="https://www.linkedin.com/company/103165743"
