12978f4689ed9a0d036819ddce505c3263f4e719
Gyom03 <guillaumerichard03@gmail.com>
2024-05-13 11:30:51 +0200

    test

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,11 @@
+"use client"
 import React from "react"
 import Image from "next/image"
 import Link from "next/link"
-
+import { usePathname } from "next/navigation"
 function Navbar() {
+  const path = usePathname()
+  if (path === "/hacked") return null
   return (
     <header className="px-4 lg:px-6 md:h-16 flex items-center bg-riskbg flex-col md:flex-row">
       <Link className="mt-3 md:mt-0 flex items-center justify-center " href="/">
