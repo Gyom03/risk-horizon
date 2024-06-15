8e1c6d31d77e1462b1ead0fdcd79c05bc39f999b
Gyom03 <guillaumerichard03@gmail.com>
2024-06-15 15:55:48 +0200

    fixed

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -14,11 +14,17 @@ function Navbar() {
   const classname = isMenuOpen ? "flex" : "hidden"
   return (
     <header className="px-4 pb-4 md:pb-0 lg:px-6 md:h-16 flex items-center bg-riskbg flex-col md:flex-row">
-      <Link className="mt-3 md:mt-0 flex items-center justify-center " href="/">
-        <Image alt="Risk Horizon logo" height="250" src="/logo_line_white.png" width="300" />
+      <Link className="mt-3 mx-20 md:mt-0 flex items-center justify-center max-w-[300px]" href="/">
+        <Image
+          alt="Risk Horizon logo"
+          height="250"
+          src="/logo_line_white.png"
+          width="300"
+          className="w-[220px] md:w-[300px]"
+        />
         <span className="sr-only">Risk Horizon asbl</span>
         <button
-          className="md:hidden fixed items-center justify-center right-5 "
+          className="md:hidden absolute items-center justify-center right-5 "
           onClick={() => setIsMenuOpen(!isMenuOpen)}
         >
           <Menu size={30} color="white" />
