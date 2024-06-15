31fec2635b69c76b843448ebd9e2b3a20a3840e3
Gyom03 <guillaumerichard03@gmail.com>
2024-06-15 15:47:12 +0200

    added hamburguer menu

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,40 +3,36 @@ import React from "react"
 import Image from "next/image"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
+import NavbarContent from "./navbarContent"
+import { useState } from "react"
+import { Menu } from "lucide-react"
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
   const path = usePathname()
   if (path === "/hacked") return null
+
+  const classname = isMenuOpen ? "flex" : "hidden"
   return (
-    <header className="px-4 lg:px-6 md:h-16 flex items-center bg-riskbg flex-col md:flex-row">
+    <header className="px-4 pb-4 md:pb-0 lg:px-6 md:h-16 flex items-center bg-riskbg flex-col md:flex-row">
       <Link className="mt-3 md:mt-0 flex items-center justify-center " href="/">
         <Image alt="Risk Horizon logo" height="250" src="/logo_line_white.png" width="300" />
         <span className="sr-only">Risk Horizon asbl</span>
+        <button
+          className="md:hidden fixed items-center justify-center right-5 "
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          <Menu size={30} color="white" />
+        </button>
       </Link>
-      <nav className="md:ml-auto flex gap-4 sm:gap-6 flex-col md:flex-row mt-6 md:mt-0 items-center md:items-start mb-4 md:mb-0">
-        <Link className="text-sm font-medium hover:underline underline-offset-4 text-white cursor-pointer" href="/">
-          Accueil
-        </Link>
-        <Link
-          className="text-sm font-medium hover:underline underline-offset-4 text-white cursor-pointer"
-          href="/Missions"
-        >
-          Missions
-        </Link>
-        <Link
-          className="text-sm font-medium hover:underline underline-offset-4 text-white cursor-pointer"
-          href="/A-propos"
-        >
-          A propos
-        </Link>
-        <Link
-          className="text-sm font-medium hover:underline underline-offset-4 text-white cursor-pointer"
-          href="/Contact"
-        >
-          Contact
-        </Link>
-        <Link className="text-sm font-medium hover:underline underline-offset-4 text-white cursor-pointer" href="/OSI">
-          OSI 2024
-        </Link>
+      <nav className="w-full ">
+        <NavbarContent className=" hidden md:flex md:ml-auto  justify-end gap-4 sm:gap-6 flex-col md:flex-row mt-6 md:mt-0 items-center md:items-start mb-4 md:mb-0" />
+
+        <NavbarContent
+          className={
+            classname +
+            "   md:hidden md:ml-auto  justify-end gap-4 sm:gap-6 flex-col md:flex-row mt-6 md:mt-0 items-center md:items-start mb-4 md:mb-0"
+          }
+        />
       </nav>
     </header>
   )
