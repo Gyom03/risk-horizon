a10b6b5a007ec5e0af7e3b93f3c87392a78a21a0
Maxime Curon <114878203+bterpperoni@users.noreply.github.com>
2024-09-04 06:38:07 +0200

    fix ui navbar ..

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,72 +1,126 @@
-"use client"
-import React, { useState } from "react"
-import Image from "next/image"
-import { usePathname, useRouter } from "next/navigation"
-import { Disclosure, DisclosureButton, DisclosurePanel, Transition } from "@headlessui/react"
-import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline"
-import { useEffect, useMemo } from "react"
-import { SignedOut, SignInButton, SignedIn, UserButton, useUser } from "@clerk/nextjs"
-import Link from "next/link"
-import { Button } from "./ui/button"
-import { Sheet, SheetTrigger, SheetContent } from "./ui/sheet"
-import { Menu } from "lucide-react"
+'use client'
+import React, { useState } from 'react'
+import Image from 'next/image'
+import { usePathname, useRouter } from 'next/navigation'
+import { useEffect, useMemo } from 'react'
+import {
+  SignedOut,
+  SignInButton,
+  SignedIn,
+  UserButton,
+  useUser
+} from '@clerk/nextjs'
+import Link from 'next/link'
+import { Button } from './ui/button'
+import { Sheet, SheetTrigger, SheetContent } from './ui/sheet'
+import { Menu } from 'lucide-react'
+
 const navigation = [
-  { name: "Accueil", href: "/", current: false, private: false },
-  { name: "Mission", href: "/Missions", current: false, private: false },
-  { name: "À propos", href: "/about", current: false, private: false },
-  { name: "Contact", href: "/Contact", current: false, private: false },
-  { name: "OSI 2024", href: "/OSI", current: false, private: false },
-  { name: "Dashboard", href: "/dashboard", current: false, private: true },
+  { name: 'Accueil', href: '/', current: false, private: false },
+  { name: 'À propos', href: '/about', current: false, private: false },
+  { name: 'Contact', href: '/Contact', current: false, private: false },
+  { name: 'OSI 2024', href: '/OSI', current: false, private: false },
+  { name: 'Dashboard', href: '/dashboard', current: false, private: true }
 ]
 
-function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(" ")
+function classNames (...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
 }
 
-function Navbar() {
+function Navbar () {
   const router = useRouter()
+
+  // Get user from clerk
+  const { user } = useUser()
 
   const [isOpen, setIsOpen] = useState(false)
 
-  const navItems = [
-    { name: "Accueil", href: "/", current: false, private: false },
-    { name: "À propos", href: "/about", current: false, private: false },
-    { name: "Contact", href: "/Contact", current: false, private: false },
-    { name: "OSI 2024", href: "/OSI", current: false, private: false },
-  ]
+  // Display bg of button when user is on the page
+  useEffect(() => {
+    navigation.map(item => {
+      if (window.location.pathname === item.href) {
+        setIsOpen(false)
+        item.current = true
+      } else {
+        item.current = false
+      }
+    })
+  }, [])
 
   const path = usePathname()
-  if (path === "/hacked") return null
+  if (path === '/hacked') return null
   // const classname = isMenuOpen ? "flex" : "hidden"
   return (
-    <nav className="bg-riskbg h-16">
-      <div className=" mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
-          <div className="flex-shrink-0">
+    <nav className='bg-riskbg h-16'>
+      <div className=' mx-auto px-4 sm:px-6 lg:px-8'>
+        <div className='flex items-center justify-between h-16'>
+          <div className='flex-shrink-0'>
             <Image
-              alt="Risk Horizon logo"
-              src="/logo_line_white.png"
-              width="280"
-              height="180"
-              className="mr-5 lg:w-[30vw]!important md:w-[15vw]!important ml-6 md:ml-5 h-auto cursor-pointer"
-              onClick={() => router.push("/")}
+              alt='Risk Horizon logo'
+              src='/logo_line_white.png'
+              width='250'
+              height='150'
+              className=' lg:w-[30vw]!important md:w-[15vw]!important h-auto cursor-pointer'
+              onClick={() => router.push('/')}
             />
           </div>
-          <div className="hidden md:block">
-            <div className="ml-10 flex items-baseline space-x-4">
-              {navItems.map((item) => (
+          <div className='hidden md:block'>
+            <div className='ml-10 flex items-baseline space-x-4'>
+              {/* {navItems.map(item => (
                 <Link
                   key={item.name}
                   href={item.href}
-                  className="text-white hover:underline  px-3 mr-1 py-2 text-[18px] text-nowrap font-medium"
+                  className={classNames(
+                    item.current
+                      ? 'bg-[#5f3494] text-white'
+                      : 'text-white hover:bg-gray-700 hover:text-white',
+                    'rounded-sm px-3 mr-1 py-2 text-[18px] text-nowrap font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
                 >
                   {item.name}
                 </Link>
-              ))}
-              <div className="translate-y-[6px]">
+              ))} */}
+
+              {navigation.map(item =>
+                // Verify if displayed for all users
+                !item.private ? (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className={classNames(
+                      item.current
+                        ? 'bg-[#5f3494] text-white'
+                        : 'text-white hover:bg-gray-700 hover:text-white',
+                      'rounded-sm px-3 mr-1 py-2 text-[18px] text-nowrap font-medium'
+                    )}
+                    aria-current={item.current ? 'page' : undefined}
+                  >
+                    {item.name}
+                    {/* {!item.private ? (item.name):(user ? item.name : '')} */}
+                  </a>
+                ) : user ? (
+                  <Link
+                    key={item.name}
+                    href={item.href}
+                    className={classNames(
+                      item.current
+                        ? 'bg-[#5f3494] text-white'
+                        : 'text-white hover:bg-gray-700 hover:text-white',
+                      'rounded-sm px-3 mr-1 py-2 text-[18px] text-nowrap font-medium'
+                    )}
+                    aria-current={item.current ? 'page' : undefined}
+                  >
+                    {item.name}
+                  </Link>
+                ) : null
+              )}
+              <div className='text-white w-max cursor-pointer rounded-md p-2 hover:bg-gray-700 flex border-2 items-center'>
                 {/* SignedOut match to user disconnected */}
                 <SignedOut>
-                  <SignInButton forceRedirectUrl={"/dashboard/"}>Se connecter</SignInButton>
+                  <SignInButton forceRedirectUrl={'/dashboard/'}>
+                    Se connecter
+                  </SignInButton>
                 </SignedOut>
                 {/* SignIn match to user connected */}
                 <SignedIn>
@@ -75,32 +129,70 @@ function Navbar() {
               </div>
             </div>
           </div>
-          <div className="md:hidden flex items-center">
+          <div className='md:hidden flex items-center'>
             <Sheet open={isOpen} onOpenChange={setIsOpen}>
               <SheetTrigger asChild>
-                <Button variant="outline" size="icon" className="md:hidden">
-                  <Menu className="h-6 w-6" />
-                  <span className="sr-only">Open menu</span>
+                <Button variant='outline' size='icon' className='md:hidden'>
+                  <Menu className='h-6 w-6' />
+                  <span className='sr-only'>Open menu</span>
                 </Button>
               </SheetTrigger>
-              <SheetContent side="right" className="bg-riskbg">
-                <div className="flex flex-col space-y-4 mt-4">
-                  {navItems.map((item) => (
+              <SheetContent side='right' className='bg-riskbg'>
+                <div className='flex flex-col space-y-4 mt-4'>
+                  {/* {navItems.map(item => (
                     <Link
                       key={item.name}
                       href={item.href}
-                      className="text-white hover:underline  px-3 mr-1 py-2 text-[18px] text-nowrap font-medium"
+                      className={classNames(
+                        item.current
+                          ? 'bg-[#5f3494] text-white'
+                          : 'text-white hover:bg-gray-700 hover:text-white',
+                        'rounded-sm px-3 mr-1 py-2 text-[18px] text-nowrap font-medium'
+                      )}
                       onClick={() => setIsOpen(false)}
+                      aria-current={item.current ? 'page' : undefined}
                     >
                       {item.name}
                     </Link>
-                  ))}
-                  <div className="scale-150 translate-x-24  mt-14">
-                    {/* SignedOut match to user disconnected */}
+                  ))} */}
+                  {navigation.map(item =>
+                    !item.private ? (
+                      <Link
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current
+                            ? 'bg-[#5f3494] text-white'
+                            : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          'rounded-md px-3 py-2 text-base font-medium flex justify-center'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </Link>
+                    ) : user ? (
+                      <Link
+                        key={item.name}
+                        as='a'
+                        href={item.href}
+                        className={classNames(
+                          item.current
+                            ? 'bg-[#5f3494] text-white'
+                            : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          'rounded-md px-3 py-2 text-base font-medium flex justify-center'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </Link>
+                    ) : null
+                  )}
+                  <div className='text-white w-full cursor-pointer hover:bg-gray-700 flex border-t-2 px-2 pt-1 flex-shrink-0 text-center justify-center'>
                     <SignedOut>
-                      <SignInButton forceRedirectUrl={"/dashboard/"}>Se connecter</SignInButton>
+                      <SignInButton forceRedirectUrl={'/dashboard/'}>
+                        Se connecter
+                      </SignInButton>
                     </SignedOut>
-                    {/* SignIn match to user connected */}
                     <SignedIn>
                       <UserButton />
                     </SignedIn>
@@ -116,3 +208,191 @@ function Navbar() {
 }
 
 export default Navbar
+
+// 'use client'
+// import React from 'react'
+// import Image from 'next/image'
+// import { usePathname, useRouter } from 'next/navigation'
+// import {
+//   Disclosure,
+//   DisclosureButton,
+//   DisclosurePanel,
+//   Transition
+// } from '@headlessui/react'
+// import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+// import { useEffect, useMemo } from 'react'
+// import {
+//   SignedOut,
+//   SignInButton,
+//   SignedIn,
+//   UserButton,
+//   useUser
+// } from '@clerk/nextjs'
+
+// const navigation = [
+//   { name: 'Accueil', href: '/', current: false, private: false },
+//   { name: 'Mission', href: '/Missions', current: false, private: false },
+//   { name: 'À propos', href: '/about', current: false, private: false },
+//   { name: 'Contact', href: '/Contact', current: false, private: false },
+//   { name: 'OSI 2024', href: '/OSI', current: false, private: false },
+//   { name: 'Dashboard', href: '/dashboard', current: false, private: true }
+// ]
+
+// function classNames (...classes: string[]) {
+//   return classes.filter(Boolean).join(' ')
+// }
+
+// function Navbar () {
+//   const router = useRouter()
+//   const { user } = useUser()
+
+// useEffect(() => {
+//   navigation.map(item => {
+//     if (window.location.pathname === item.href) {
+//       item.current = true
+//     } else {
+//       item.current = false
+//     }
+//   })
+// }, [])
+//   const path = usePathname()
+//   if (path === '/hacked') return null
+//   // const classname = isMenuOpen ? "flex" : "hidden"
+//   return (
+//     <div className=' bg-riskbg  '>
+//       <Disclosure as='nav' className='bg-[#040437]  absolute top-0 w-full z-1'>
+//         {({ open }: { open: boolean }) => (
+//           <>
+//             <div className=' '>
+//               <div className='relative flex h-16 items-center justify-between'>
+//                 <div className='absolute inset-y-0 left-0 flex items-center sm:hidden'>
+//                   {/* Mobile menu button*/}
+//                   <DisclosureButton className='m-2 absolute inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-[#040437]hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white'>
+//                     <span className='absolute -inset-0.5' />
+//                     <span className='sr-only'>Open main menu</span>
+//                     {open ? (
+//                       <XMarkIcon className='block h-6 w-6' aria-hidden='true' />
+//                     ) : (
+//                       <Bars3Icon className='block h-6 w-6' aria-hidden='true' />
+//                     )}
+//                   </DisclosureButton>
+//                 </div>
+//                 <div className='flex flex-1 items-center justify-center sm:items-stretch sm:justify-start'>
+//                   <div className='flex items-center'>
+//                     <Image
+//                       alt='Risk Horizon logo'
+//                       src='/logo_line_white.png'
+//                       width='250'
+//                       height='150'
+//                       className='mr-5 lg:w-[30vw]!important md:w-[15vw]!important ml-6 md:ml-5 h-auto cursor-pointer'
+//                       onClick={() => router.push('/')}
+//                     />
+//                   </div>
+// <div className='hidden w-full w-min-[90px] relative right-3 sm:ml-6 sm:block mr-4'>
+//   <div className='flex flex-shrink-1 w-full ml-4 justify-end items-center '>
+//     {navigation.map(item =>
+//       !item.private ? (
+//         <a
+//           key={item.name}
+//           href={item.href}
+//           className={classNames(
+//             item.current
+//               ? 'bg-[#5f3494] text-white'
+//               : 'text-white hover:bg-gray-700 hover:text-white',
+//             'rounded-sm px-3 mr-1 py-2 text-[18px] text-nowrap font-medium'
+//           )}
+//           aria-current={item.current ? 'page' : undefined}
+//         >
+//           {item.name}
+//           {/* {!item.private ? (item.name):(user ? item.name : '')} */}
+//         </a>
+//       ) : user ? (
+//         <a
+//           key={item.name}
+//           href={item.href}
+//           className={classNames(
+//             item.current
+//               ? 'bg-[#5f3494] text-white'
+//               : 'text-white hover:bg-gray-700 hover:text-white',
+//             'rounded-sm px-3 mr-1 py-2 text-[18px] text-nowrap font-medium'
+//           )}
+//           aria-current={item.current ? 'page' : undefined}
+//         >
+//           {item.name}
+//         </a>
+//       ) : null
+//     )}
+//     <div className='text-white w-max cursor-pointer rounded-md p-2 hover:bg-gray-700 flex border-2 items-center'>
+//       {/* SignedOut match to user disconnected */}
+//       <SignedOut>
+//         <SignInButton forceRedirectUrl={'/dashboard/'}>
+//           Se connecter
+//         </SignInButton>
+//       </SignedOut>
+//       {/* SignIn match to user connected */}
+//       <SignedIn>
+//         <UserButton />
+//       </SignedIn>
+//     </div>
+//   </div>
+// </div>
+//                 </div>
+//               </div>
+//             </div>
+
+//             <DisclosurePanel className='md:hidden'>
+//               <div className='space-y-1 px-2  pb-3 pt-2'>
+//                 {navigation.map(item =>
+//                   !item.private ? (
+//                     <DisclosureButton
+//                       key={item.name}
+//                       as='a'
+//                       href={item.href}
+//                       className={classNames(
+//                         item.current
+//                           ? 'bg-[#5f3494] text-white'
+//                           : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+//                         'rounded-md px-3 py-2 text-base font-medium flex justify-center'
+//                       )}
+//                       aria-current={item.current ? 'page' : undefined}
+//                     >
+//                       {item.name}
+//                     </DisclosureButton>
+//                   ) : user ? (
+//                     <DisclosureButton
+//                       key={item.name}
+//                       as='a'
+//                       href={item.href}
+//                       className={classNames(
+//                         item.current
+//                           ? 'bg-[#5f3494] text-white'
+//                           : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+//                         'rounded-md px-3 py-2 text-base font-medium flex justify-center'
+//                       )}
+//                       aria-current={item.current ? 'page' : undefined}
+//                     >
+//                       {item.name}
+//                     </DisclosureButton>
+//                   ) : null
+//                 )}
+//                 <div className='text-white w-full cursor-pointer hover:bg-gray-700 flex border-t-2 px-2 pt-1 flex-shrink-0 text-center justify-center'>
+//                   <SignedOut>
+//                     <SignInButton forceRedirectUrl={'/dashboard/'}>
+//                       Se connecter
+//                     </SignInButton>
+//                   </SignedOut>
+//                   <SignedIn>
+//                     <UserButton />
+//                   </SignedIn>
+//                 </div>
+//               </div>
+//             </DisclosurePanel>
+//           </>
+//         )}
+//       </Disclosure>
+
+//     </div>
+//   )
+// }
+
+// export default Navbar
